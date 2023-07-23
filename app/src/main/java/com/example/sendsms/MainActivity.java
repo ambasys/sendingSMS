0c10f458138e296e038bf3ef3e01c8214c6023da
ambasys <140305049+ambasys@users.noreply.github.com>
2023-07-23 04:54:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/sendsms/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.sendsms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+EditText message,phone_number;
+Button send;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        message=findViewById(R.id.editText);
+        phone_number=findViewById(R.id.editText2);
+        send=findViewById(R.id.button);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendSMS();
+            }
+        });
+    }
+
+    private void sendSMS() {
+        String msg=message.getText().toString();
+        String phoneNo=phone_number.getText().toString();
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("sms:"+phoneNo));
+        intent.putExtra("sms_body",msg);
+        startActivity(intent);
+    }
+}
